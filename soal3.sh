4a2b81a01a242579611ff6f0ebf78b8aed056fcc
Christian Andrew <christianandrew7449@gmail.com>
2020-02-21 13:50:28 +0700

    Update soal 3

--- soal3.sh
+++ soal3.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
-for aduh in {1..28}
+totalfile=`ls | grep "pdkt_kusuma" | cut -d "_" -f 3 | sort -n | tail -1`
+
+if [[ $totalfile =~ [:digit:] ]]
+then
+	$totalfile = 0
+fi
+
+b=`expr $totalfile + 1`
+c=`expr $totalfile + 28`
+
+for((a=b;a<=c;a++))
 do
-wget "https://loremflickr.com/320/240/cat" -a wget.log -O "pdkt_kusuma_$aduh"
+wget "https://loremflickr.com/320/240/cat" -a wget.log -O pdkt_kusuma_$a
 done
+
+grep "Location" wget.log >> location.log
